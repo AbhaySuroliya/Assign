503ca2b7c7ab58932f549470a2b74cefc22df35d
Abhay Suroliya <abhaysuroliya@gmail.com>
2022-09-19 13:26:34 +0530

    initial

---
+++ src/com/arrayMatrix/Matrix.java
@@ -0,0 +1,41 @@
+package com.arrayMatrix;
+import java.util.Scanner;
+public class Matrix {	
+	public static void main(String[] args) {
+		Scanner scan=new Scanner(System.in);
+		System.out.println("Enter dimention");
+    	int x=scan.nextInt();
+    	int y=scan.nextInt();
+    	int arr[][]=new int[x][y];
+    	for(int i=0;i<x;i++)
+    	{
+    		for(int j=0;j<y;j++)
+    		{
+    			arr[i][j]=scan.nextInt();
+    		}
+    	}
+    	int i, k = 0, l = 0;
+      while (k < y && l < x) {
+            for (i = l; i < x; ++i) {
+                System.out.print(arr[k][i] + " ");
+            }
+            k++;
+            for (i = k; i < y; ++i) {
+                System.out.print(arr[i][x - 1] + " ");
+            }
+            x--;
+            if (k < y) {
+                for (i = x - 1; i >= l; --i) {
+                    System.out.print(arr[y - 1][i] + " ");
+                }
+                y--;
+            }
+            if (l < x) {
+                for (i = y - 1; i >= k; --i) {
+                    System.out.print(arr[i][l] + " ");
+                }
+                l++;
+            }
+    }
+ }
+}
---
+++ src/com/string/StringMain.java
@@ -0,0 +1,29 @@
+package com.string;
+import java.util.Scanner;
+public class StringMain {
+
+	public static void main(String[] args) {
+		Scanner scan=new Scanner(System.in);
+		int a[]=new int[26];
+		 System.out.println("Enter String");
+         String sen=scan.next();
+        
+         for(int i=0;i<sen.length();i++)
+         {
+        	 a[sen.charAt(i)-'a']++;
+         }
+         String r="";
+         for(int i=0;i<sen.length();i++)
+         {
+        	 if(a[sen.charAt(i)-'a']==0)
+        		 continue;
+        	 else {
+        	 r=r+sen.charAt(i)+(a[sen.charAt(i)-'a']);
+        	 a[sen.charAt(i)-'a']=0;
+        	 }
+         }
+         System.out.println(r);
+	}
+
+}
+
